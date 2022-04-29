9ecfc1898990149467bef8744da5a29c58968bcd
Alex Cannon <86203152+theVikingMan@users.noreply.github.com>
2022-04-29 15:17:46 -0700

    (Finalizing edits for express server config)

--- server/app.js
+++ server/app.js
@@ -7,6 +7,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/loaderio-525acf56a56bae826adb0ce47e4f2eb5', async (req, res) => {
+  try {
+    res.send('loaderio-525acf56a56bae826adb0ce47e4f2eb5');
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
+});
+
 app.use('/reviews', routes);
 
 module.exports = app;
